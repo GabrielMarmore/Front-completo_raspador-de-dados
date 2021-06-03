b7e652abdbfe3b9b4005b8f5efff2a49f044cd84
Gabriel Mármore <gabrielpiraja@hotmail.com>
2021-06-03 12:30:57 -0300

    Update raspar() e pegarDados()
    Ao inves da url ir direto no fetch agora usa um array com as urls numa string formatada.
    Uso de if para verificação do doom.
    Volta do btn fé

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
-function raspar(documento) {
-    const subtitle = document.createElement("p");
+function raspar(documento){
+    if(documento.title=="Aatrox - Habilidades e skins dos campeões de League of Legends - Millenium"){
+        const subtitle = document.createElement("p");
+
+        documento.querySelectorAll("div").forEach(div => {
+            if(div.classList=="article__title")document.querySelector("h1").innerHTML=div.innerHTML;//Verifiquei e somente essa div possue essa class
+        });
+
+        subtitle.innerHTML=documento.querySelector(".article__chapo").innerHTML;subtitle.style.fontSize="18px";
+        document.querySelector("header").appendChild(subtitle);
+    }
+    /*
     var divNossa = document.createElement("div");
-    //documento.querySelectorAll(".style__Desc-sc-1o884zt-9 hZPZqS").forEach(div => {
-    documento.querySelectorAll("div").forEach(div => {
-        if(div.classList=="article__title")document.querySelector("h1").innerHTML=div.innerHTML;//Verifiquei e somente essa div possue essa class
-    });
-    subtitle.innerHTML=documento.querySelector(".article__chapo").innerHTML;subtitle.style.fontSize="18px";
-
     const divPage = document.querySelector("section div:last-of-type")
-    document.querySelector("header").appendChild(subtitle);
-    //divPage.appendChild(divNossa);
+    divPage.appendChild(divNossa);*/
 }
 
-function pegarDados() {
-    //fetch("https://cors-anywhere.herokuapp.com//https://br.leagueoflegends.com/pt-br/champions/aatrox/")
-    fetch("https://cors-anywhere.herokuapp.com/https://br.millenium.gg/jogos/jogo-12/entidade-4083")
-        .then(resp => resp.text())
-        .then(pg => { 
-            let dom = new DOMParser();
-            let documento = dom.parseFromString(pg, "text/html");
-            raspar(documento);
-        })
-        .catch(e => document.write(e));
+function pegarDados(){
+    const urls =["https://br.millenium.gg/jogos/jogo-12/entidade-4083","https://leagueoflegends.fandom.com/wiki/Darkin"]
+    for(ix in urls){
+        fetch(`https://cors-anywhere.herokuapp.com/${urls[ix]}`)
+            .then(resp => resp.text())
+            .then(pg => { 
+                let dom = new DOMParser();
+                let documento = dom.parseFromString(pg, "text/html");
+                raspar(documento);
+            })
+            .catch(e => document.write(e));
+    }
 }
+function evento(){document.querySelector("button").addEventListener("click", pegarDados);}
 
-//function evento() {document.querySelector("button").addEventListener("click", pegarDados);}
-
-pegarDados()// window.onload carrega todo o doom depois aplica a função, dando f5 fica evidente a mudança de el
+//pegarDados()// window.onload carrega todo o doom depois aplica a função, dando f5 fica evidente a mudança de el
 //window.onload = pegarDados
+window.onload = evento;
 
 //https://cors-anywhere.herokuapp.com/linkdosite
